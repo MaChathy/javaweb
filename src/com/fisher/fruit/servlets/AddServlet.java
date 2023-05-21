041e1530c94821b43756ec7a67b13e0a4ae99fa7
fisher <fisher@macarthy.com>
2023-05-21 14:27:13 +0800

    add DomeServlet01

--- src/com/fisher/fruit/servlets/AddServlet.java
+++ src/com/fisher/fruit/servlets/AddServlet.java
@@ -3,12 +3,15 @@ package com.fisher.fruit.servlets;
 import com.fisher.fruit.dao.FruitDao;
 import com.fisher.fruit.dao.impl.FruitDaoImpl;
 import com.fisher.fruit.pojo.Fruit;
+import org.junit.Test;
 
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *  Add.do 工件
@@ -16,6 +19,7 @@ import java.io.IOException;
  * @version 1.0.1 2023/5/20 - 19:37
  */
 public class AddServlet extends HttpServlet {
+
     /**
      *
      * @param request
@@ -26,14 +30,20 @@ public class AddServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws
             IOException, ServletException {
+        //post方式下设置编码，防止中文乱码
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=UTF-8");
         String fName = request.getParameter("fName");
         int price =Integer.parseInt(request.getParameter("price"));
         int fCount = Integer.parseInt(request.getParameter("fCount"));
         String remark = request.getParameter("remark");
+        PrintWriter writer = response.getWriter();
 
         System.out.println("名称："+fName+"\n价格："+price+"\n库存："+fCount+"\n备注："+remark);
-//        FruitDao fruitDao = new FruitDaoImpl();
-//        boolean flag = fruitDao.addFruit(new Fruit(0, fName, price, fCount, remark));
-//        System.out.println(flag?"添加成功":"添加失败");
+        writer.write("接收信息成功");
+        writer.close();
+        //FruitDao fruitDao = new FruitDaoImpl();
+        //boolean flag = fruitDao.addFruit(new Fruit(0, fName, price, fCount, remark));
+        //System.out.println(flag?"添加成功":"添加失败");
     }
 }
--- src/com/fisher/servlets/README.md
+++ src/com/fisher/servlets/README.md
@@ -1,6 +1,29 @@
 **1.设置编码**
+    
+    1)tomcat8之前，设置编码：
+
+        ①get请求方式：
+        e.g. String name = request.getParameter("name")");
+             //将字符串打散成字节数组
+             byte[] bytes = name.getBytes("ISO-8859-1");
+             //将字节数组按照设定的编码重新组装成字符串
+             name = new String(bytes,"UTF-8");
+
+        ②post请求方式：
+        e.g.  //post方式下设置编码，防止中文乱码
+              request.setCharacterEncoding("utf-8");
+    
+    2)tomcat8开始，设置编码时只需对post请求方式设置编码
+        e.g.  request.setCharacterEncoding("utf-8");
+
+    **设置编码必须在所有的获取参数动作之前**
 
 **2.servlet的继承关系**
+
+    javax.servlet.Servlet接口
+        javax.servlet.GenericServlet抽象类 
+            javax.servlet.http.HttpServlet
+            
 
 **3.servlet的生命周期**
 
